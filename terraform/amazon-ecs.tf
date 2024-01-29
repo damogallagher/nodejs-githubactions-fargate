57e58334411b8321fa9b9b2a0eec593a9d12ef16
Damien Gallagher <damien.gallagher@gmail.com>
2024-01-29 22:20:26 +0000

    Update to try deploying to fargate

--- terraform/amazon-ecs.tf
+++ terraform/amazon-ecs.tf
@@ -9,8 +9,21 @@ name = "${var.company_name}-cluster"
 
 resource "aws_ecs_task_definition" "fargate_task" {
   family                   = "${var.company_name}-fargate-task"
-  network_mode              = "awsvpc"
-  container_definitions = file("task-definition.json")
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"  # CPU units (1 vCPU = 256 CPU units)
+  memory                   = "512"  # Memory in MiB
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name  = "${var.company_name}-container"
+    image = "nginx:latest"
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+  }])
 }
 
 resource "aws_iam_role" "ecs_execution_role" {
@@ -52,9 +65,6 @@ output "ecs_service_name" {
   value = "${var.company_name}-fargate-service"
 }
 
-output "ecs_task_definition_arn" {
-    value = aws_ecs_task_definition.fargate_task.arn
-}
 output "ecs_task_definition_name" {
-    value = "${var.company_name}-fargate-task"
+    value = "${var.company_name}-fargate-task-definition"
 }
