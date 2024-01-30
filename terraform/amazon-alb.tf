0205c54c75dffaa2217ec45c001b40f18d22c840
Damien Gallagher <damien.gallagher@gmail.com>
2024-01-30 17:07:50 +0000

    Update

--- terraform/amazon-alb.tf
+++ terraform/amazon-alb.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "fargate_alb_sg" {
 
 # Create an ALB target group
 resource "aws_lb_target_group" "fargate_target_group" {
-  name     = "${var.company}-fargate-target-group"
+  name     = "${var.company}-fargate-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.vpc.id
