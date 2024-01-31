0bb0007d9e9b835b996b9c0e1c24cdf95e5e21d6
Damien Gallagher <damien.gallagher@gmail.com>
2024-01-31 12:10:17 +0000

    Update

--- terraform/amazon-vpc.tf
+++ terraform/amazon-vpc.tf
@@ -85,12 +85,16 @@ resource "aws_route_table" "public_1" {
 resource "aws_route_table" "public_2" {
   vpc_id = aws_vpc.vpc.id
 }
-resource "aws_route" "public_internet_gateway" {
-  route_table_id         = aws_route_table.public.id
+resource "aws_route" "public_internet_gateway_1" {
+  route_table_id         = aws_route_table.public[0].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.ig.id
 }
-
+resource "aws_route" "public_internet_gateway_2" {
+  route_table_id         = aws_route_table.public[1].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ig.id
+}
 
 
 /* Route table associations */
